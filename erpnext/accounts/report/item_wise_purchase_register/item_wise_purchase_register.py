0ebd297d8ac6990ef94fe6d760a4d95842651cd2
ShashaQin <shasha.qin@ia-group.com.au>
2015-07-27 14:30:13 +0800

    Update item_wise_purchase_register.py
    Associated with Task00043, add Supplier Name after Supplier column in Item Wise Purchase Register report.

--- erpnext/accounts/report/item_wise_purchase_register/item_wise_purchase_register.py
+++ erpnext/accounts/report/item_wise_purchase_register/item_wise_purchase_register.py
@@ -19,7 +19,7 @@ def execute(filters=None):
 	data = []
 	for d in item_list:
 		expense_account = d.expense_account or aii_account_map.get(d.company)
-		row = [d.item_code, d.item_name, d.item_group, d.parent, d.posting_date,
+		row = [d.item_code, d.item_name, d.item_group, d.parent, d.posting_date,d.supplier,
 			d.supplier_name, d.credit_to, d.project_name, d.company, d.purchase_order,
 			d.purchase_receipt, expense_account, d.qty, d.base_rate, d.base_amount]
 		for tax in tax_accounts:
@@ -35,7 +35,7 @@ def execute(filters=None):
 
 def get_columns():
 	return [_("Item Code") + ":Link/Item:120", _("Item Name") + "::120", _("Item Group") + ":Link/Item Group:100",
-		_("Invoice") + ":Link/Purchase Invoice:120", _("Posting Date") + ":Date:80", _("Supplier") + "::120",
+		_("Invoice") + ":Link/Purchase Invoice:120", _("Posting Date") + ":Date:80", _("Supplier") + ":Link/Supplier:120", _("Supplier Name") + "::120",
 		_("Supplier Account") + ":Link/Account:120", _("Project") + ":Link/Project:80", _("Company") + ":Link/Company:100",
 		_("Purchase Order") + ":Link/Purchase Order:100", _("Purchase Receipt") + ":Link/Purchase Receipt:100",
 		_("Expense Account") + ":Link/Account:140", _("Qty") + ":Float:120", _("Rate") + ":Currency:120",
