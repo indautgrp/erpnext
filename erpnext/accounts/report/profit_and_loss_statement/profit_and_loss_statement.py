912776759ec38f13b7c4dfad2f567d353eedf80c
robertschouten <robert_schouten@live.com>
2015-11-30 12:21:38 +0800

    Update profit_and_loss_statement.py
    redo Customization #55 for #55

--- erpnext/accounts/report/profit_and_loss_statement/profit_and_loss_statement.py
+++ erpnext/accounts/report/profit_and_loss_statement/profit_and_loss_statement.py
@@ -10,8 +10,8 @@ from erpnext.accounts.report.financial_statements import (get_period_list, get_c
 def execute(filters=None):
 	period_list = get_period_list(filters.fiscal_year, filters.periodicity)
 
-	income = get_data(filters.company, "Income", "Credit", period_list, ignore_closing_entries=True)
-	expense = get_data(filters.company, "Expense", "Debit", period_list, ignore_closing_entries=True)
+	income = get_data(filters.company, "Income", "Credit", period_list, filters.accumulated_value, ignore_closing_entries=True)
+	expense = get_data(filters.company, "Expense", "Debit", period_list, filters.accumulated_value, ignore_closing_entries=True)
 	net_profit_loss = get_net_profit_loss(income, expense, period_list)
 
 	data = []
@@ -20,19 +20,27 @@ def execute(filters=None):
 	if net_profit_loss:
 		data.append(net_profit_loss)
 
-	columns = get_columns(period_list)
+	columns = get_columns(filters.periodicity,period_list,filters.accumulated_value)
 
 	return columns, data
 
 def get_net_profit_loss(income, expense, period_list):
 	if income and expense:
+		total_column=0
 		net_profit_loss = {
 			"account_name": "'" + _("Net Profit / Loss") + "'",
 			"account": None,
 			"warn_if_negative": True
 		}
+		has_value = False
 
 		for period in period_list:
 			net_profit_loss[period.key] = flt(income[-2][period.key] - expense[-2][period.key], 3)
 
-		return net_profit_loss
+			if net_profit_loss[period.key]:
+				has_value=True
+			total_column=total_column+net_profit_loss[period.key]
+			net_profit_loss["total"]=total_column
+		if has_value:
+
+			return net_profit_loss
