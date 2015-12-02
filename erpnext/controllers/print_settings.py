abce002bdc6f4604e1f2ee2e5d962904675544fb
robert schouten <robert_schouten@live.com>
2015-12-02 12:00:47 +0800

    redo changes #24 for #24
    redo changes #24 for #24

--- erpnext/controllers/print_settings.py
+++ erpnext/controllers/print_settings.py
@@ -4,6 +4,9 @@
 def print_settings_for_item_table(doc):
 	doc.print_templates = {
 		"description": "templates/print_formats/includes/item_table_description.html",
-		"qty": "templates/print_formats/includes/item_table_qty.html"
+		"qty": "templates/print_formats/includes/item_table_qty.html",
+		"price_list_rate": "templates/print_formats/includes/item_table_price_list_rate.html",
+		"rate": "templates/print_formats/includes/item_table_rate.html",
+		"amount": "templates/print_formats/includes/item_table_rate.html"
 	}
 	doc.hide_in_print_layout = ["item_code", "item_name", "image", "uom", "stock_uom"]
