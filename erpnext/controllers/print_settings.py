3c0ea50a3bd5d4e0a04553f92694d58afebb757b
IAGAdmin <finance@ia-group.com.au>
2016-01-06 10:44:00 +0800

    Update print_settings.py

--- erpnext/controllers/print_settings.py
+++ erpnext/controllers/print_settings.py
@@ -7,6 +7,6 @@ def print_settings_for_item_table(doc):
 		"qty": "templates/print_formats/includes/item_table_qty.html",
 		"price_list_rate": "templates/print_formats/includes/item_table_price_list_rate.html",
 		"rate": "templates/print_formats/includes/item_table_rate.html",
-		"amount": "templates/print_formats/includes/item_table_rate.html"
+		"amount": "templates/print_formats/includes/item_table_amount.html"
 	}
 	doc.hide_in_print_layout = ["item_code", "item_name", "image", "uom", "stock_uom"]
