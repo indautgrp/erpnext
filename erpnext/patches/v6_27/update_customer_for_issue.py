198daeca7aa52e947fb839fe953dd1f7b172edd1
ShashaQin <shasha.qin@ia-group.com.au>
2016-06-09 17:16:53 +0800

    Update update_customer_for_issue.py

--- erpnext/patches/v6_27/update_customer_for_issue.py
+++ erpnext/patches/v6_27/update_customer_for_issue.py
@@ -4,7 +4,7 @@ import frappe
 def execute():
 	updated_list = []
 
-	for issue in frappe.db.sql_list("""select name from `tabIssue` where contact is null or customer is null"""):
+	for issue in frappe.db.sql_list("""select name from `tabIssue` where (contact is null or contact='') or (customer is null or customer='')"""):
 		
 		issue = frappe.get_doc("Issue", issue)
 		
@@ -12,12 +12,13 @@ def execute():
 			contact = frappe.db.get_value("Contact", {"email_id": issue.raised_by}, "name")
 			customer = frappe.db.get_value("Contact", {"email_id": issue.raised_by}, "customer")
 			
-			if contact and not issue.contact:
+			if contact and (not issue.contact or issue.contact ==''):
 				updated_list.append(issue.name)
 				issue.db_set("contact", contact, update_modified=False)
 			
-			if customer and not issue.customer:
+			if customer and (not issue.customer or issue.customer ==''):
 				updated_list.append(issue.name)
 				issue.db_set("customer", customer, update_modified=False)
 
 	print "updated issue list: ", updated_list
+
