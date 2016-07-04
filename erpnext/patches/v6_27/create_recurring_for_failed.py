165d6795c2e3b173282736127c092ede57ae8f55
ShashaQin <shasha.qin@ia-group.com.au>
2016-07-04 17:04:52 +0800

    Create create_recurring_for_failed.py

---
+++ erpnext/patches/v6_27/create_recurring_for_failed.py
@@ -0,0 +1,10 @@
+import frappe
+from erpnext.controllers.recurring_document import manage_recurring_documents
+
+def execute():
+
+	frappe.db.sql("""update `tabSales Invoice` 
+				set is_recurring=1 where (docstatus=1 or docstatus=0) and next_date='2016-06-26' and is_recurring=0""")
+	
+	manage_recurring_documents("Sales Invoice", "2016-06-26")
+
