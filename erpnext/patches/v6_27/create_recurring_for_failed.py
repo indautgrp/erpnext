cb19b1ef07c18151c816dd13278346953cb0920c
ShashaQin <shasha.qin@ia-group.com.au>
2016-07-06 14:54:34 +0800

    Update create_recurring_for_failed.py

--- erpnext/patches/v6_27/create_recurring_for_failed.py
+++ erpnext/patches/v6_27/create_recurring_for_failed.py
@@ -5,7 +5,7 @@ def execute():
 
 	frappe.db.sql("""update `tabSales Invoice` 
 				set is_recurring=1 where (docstatus=1 or docstatus=0)
-				and (next_date='2016-06-25' or next_date='2016-06-26') and is_recurring=0""")
+				and (next_date='2016-06-25' or next_date='2016-06-26') and is_recurring=0 and name!='SINV-12230'""")
 	
 	manage_recurring_documents("Sales Invoice", "2016-06-25")
 	manage_recurring_documents("Sales Invoice", "2016-06-26")
