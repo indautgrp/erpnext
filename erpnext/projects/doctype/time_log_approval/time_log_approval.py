752cbabca0d491da018db90cd362465cce359c87
ERPNext <erpnext@erpnext-vm.erpnext-vm>
2016-07-14 08:04:24 +0530

    add reject action

--- erpnext/projects/doctype/time_log_approval/time_log_approval.py
+++ erpnext/projects/doctype/time_log_approval/time_log_approval.py
@@ -18,7 +18,7 @@ class TimeLogApproval(Document):
 			from
 				`tabTime Log` 
 			where
-				date_worked >= %s and date_worked <= %s and docstatus=0
+				date_worked >= %s and date_worked <= %s and docstatus=0 and workflow_state='Pending'
 				order by date_worked DESC, name DESC""" %
 				('%s', '%s'), (self.from_date, self.to_date), as_dict=1)
 
@@ -39,20 +39,56 @@ class TimeLogApproval(Document):
 			nl.quotation = d.quotation_
 
 	def approve_time_log(self):
-		time_logs = []
+		time_logs_approved = []
+		time_logs_rejected = []
+
 		for d in self.get('time_log_list'):
 
-			if d.approved:
-				time_log = frappe.get_doc('Time Log', d.time_log_id)
-				time_log.submit()
-				frappe.db.set_value("Time Log", d.time_log_id, "workflow_state", "Approved")
-				time_logs.append(d.time_log_id)
-		
+			if d.action == 'Approved':
+				if self.has_action_permission('Approve'):
+					time_log = frappe.get_doc('Time Log', d.time_log_id)
+					time_log.submit()
+					frappe.db.set_value("Time Log", d.time_log_id, "workflow_state", "Approved")
+					time_logs_approved.append(d.time_log_id)
+				else:
+					msgprint("Not permitted to Approve")
+			if d.action == 'Rejected':
+				if self.has_action_permission('Reject'):
+					time_log = frappe.get_doc('Time Log', d.time_log_id)
+					time_log.workflow_state = "Rejected"
+					time_log.save()
+					time_logs_rejected.append(d.time_log_id)
+				else:
+					msgprint("Not permitted to Reject")
+
 		self.get_details()
 
-		if time_logs:
-			msgprint("Approved time log: {0}".format(", ".join(time_logs)))
+		if time_logs_rejected or time_logs_approved:
+			if (len(time_logs_approved)>0):
+				msgprint("Approved time log: {0}".format(", ".join(time_logs_approved)))
+			if (len(time_logs_rejected)>0):
+				msgprint("Rejected time log: {0}".format(", ".join(time_logs_rejected)))
 		else:
-			msgprint(_("No time log approved"))
+			msgprint(_("No time log approved or rejected"))
+
+	def has_action_permission(self, action):
+		
+		user_roles = frappe.get_roles(frappe.session.user)
+		has_action_permission = False
+
+		for role in user_roles:
+			
+			result = frappe.db.sql("""select * from `tabWorkflow Transition` where parent='Tim log approval for Employees'
+					and action = %s and allowed = %s""", (action, role), as_dict=1)
+			if result:
+				has_action_permission = True
+				break
+
+		return has_action_permission
+			
+
+			
+		
+			
 
 			
