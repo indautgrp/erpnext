1b46dab34c431d0f8409acfc3b5564b291d65c87
robert schouten <robert_schouten@live.com>
2016-10-20 11:12:50 +0800

    d fix for project patch 89bab28

--- erpnext/patches/v7_0/rename_project_name_to_project.py
+++ erpnext/patches/v7_0/rename_project_name_to_project.py
@@ -4,6 +4,6 @@ import frappe
 from frappe.model.utils.rename_field import rename_field
 
 def execute():
-    for dt in ("GL Entry", "Journal Entry", "Journal Entry Account", "Purchase Invoice Item", "Sales Invoice", "Delivery Note", "Purchase Receipt Item", "Stock Entry"):
+    for dt in ("GL Entry", "Journal Entry Account", "Purchase Invoice Item", "Sales Invoice", "Delivery Note", "Purchase Receipt Item", "Stock Entry"):
         frappe.reload_doctype(dt)
         rename_field(dt, "project_name", "project")
